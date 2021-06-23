35076b5506e498343eef1296ab2138319dfd3831
Masasasaki <masakiasanuma@outlook.com>
2021-06-23 22:05:22 +0900

    Initial setup for new & improved portfolio

--- src/App.js
+++ src/App.js
@@ -1,26 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Heading } from '@chakra-ui/react';
+import styled from 'styled-components';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    return (
+        <div>
+            <StyledHeading>Chakra UI & styled-components work</StyledHeading>
+        </div>
+    );
+};
+
+const StyledHeading = styled(Heading)`
+    color: white;
+    background: black;
+`
 
 export default App;
