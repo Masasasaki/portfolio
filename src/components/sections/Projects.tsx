8ead771341950aac92933502793b2c721e596a3d
Masasasaki <masakiasanuma@outlook.com>
2021-06-30 17:18:33 +0900

    Enhanced project section with icons and links

--- src/components/sections/Projects.tsx
+++ src/components/sections/Projects.tsx
@@ -1,11 +1,34 @@
-import { Box, Heading, Image, Text, Button } from '@chakra-ui/react';
+import { Box, Heading, Image, Text, Button, Popover, PopoverArrow, PopoverBody, PopoverCloseButton, PopoverContent, PopoverHeader, PopoverTrigger } from '@chakra-ui/react';
 import React from 'react';
 import styled from 'styled-components';
+
 import mts from '../../images/MTS.png';
 import mangadex from '../../images/mangadexterity.png';
 import portfolio from '../../images/portfolio.png';
+import rgb from '../../images/rgb.png';
+import zappay from '../../images/zappay.png';
+
+import {
+    FaReact,
+    FaJava,
+    FaBootstrap,
+    FaSwift,
+    FaPython,
+    FaNodeJs,
+    FaHtml5,
+    FaCss3Alt,
+    FaGithub,
+    FaBus,
+    FaCode,
+} from 'react-icons/fa';
+
+import {
+    SiJavascript,
+    SiStyledComponents,
+} from 'react-icons/si';
 
 const Projects = () => {
+    const iconSize = 30;
     return (
         <Wrap>
             <Heading size='3xl'>Projects</Heading>
@@ -21,11 +44,24 @@ const Projects = () => {
                         </Description>
                     </Content>
                     <Footer>
-                        <ActionButton colorScheme='blue'>
+                        <ActionButton
+                            rightIcon={<FaGithub size={20} />}
+                            colorScheme='blackAlpha'
+                            onClick={() => {
+                                const win = window.open('https://github.com/Masasasaki/portfolio/tree/dev', '_blank')!;
+                                win.focus();
+                            }}
+                        >
                             Github Repo
                         </ActionButton>
                         <TechUsed fontSize='lg'>
-                            React Hooks, styled-components, Chakra UI
+                            <IconWrap>
+                                <FaReact size={iconSize} />
+                            </IconWrap>
+                            <IconWrap>
+                                <SiStyledComponents size={iconSize} />
+                            </IconWrap>
+                            <Text paddingLeft='5px'>Chakra UI</Text>
                         </TechUsed>
                     </Footer>
                 </ProjectEntry>
@@ -36,15 +72,32 @@ const Projects = () => {
                             Mass Transit Simulation Application
                         </Title>
                         <Description fontSize='lg'>
-                            A discrete-event simulation that allows buses to travel along different routes while transporting riders to travel along different routes while transporting riders to different stops. Download the project below and start the app by running the execute.bat file!
+                            A discrete-event simulation that allows buses to travel along different routes while transporting riders to travel along different routes while transporting riders to different stops.
                         </Description>
                     </Content>
                     <Footer>
-                        <ActionButton colorScheme='blue'>
-                            Download
-                        </ActionButton>
+                        <Popover>
+                            <PopoverTrigger>
+                                <ActionButton
+                                    rightIcon={<FaBus size={20} />}
+                                    colorScheme='blackAlpha'
+                                >
+                                    Class Project
+                                </ActionButton>
+                            </PopoverTrigger>
+                            <PopoverContent color="white" bg="black" borderColor="black">
+                                <PopoverHeader fontWeight='bolder' fontSize='xl'>
+                                    Unfotunately...
+                                </PopoverHeader>
+                                <PopoverArrow />
+                                <PopoverCloseButton />
+                                <PopoverBody fontSize='lg'>This was for a class project so the code & app is within Georgia Tech's private Github repository.</PopoverBody>
+                            </PopoverContent>
+                        </Popover>
                         <TechUsed fontSize='lg'>
-                            Java
+                            <IconWrap>
+                                <FaJava size={iconSize} />
+                            </IconWrap>
                         </TechUsed>
                     </Footer>
                 </ProjectEntry>
@@ -59,49 +112,91 @@ const Projects = () => {
                         </Description>
                     </Content>
                     <Footer>
-                        <ActionButton colorScheme='blue'>
+                        <ActionButton
+                            rightIcon={<FaGithub size={20} />}
+                            colorScheme='blackAlpha'
+                            onClick={() => {
+                                const win = window.open('https://github.com/Masasasaki/Mangadexterity', '_blank')!;
+                                win.focus();
+                            }}
+                        >
                             Github Repo
                         </ActionButton>
                         <TechUsed fontSize='lg'>
-                            React, Node, Express.js, Bootstrap
+                            <IconWrap>
+                                <FaReact size={iconSize} />
+                            </IconWrap>
+                            <IconWrap>
+                                <FaNodeJs size={iconSize} />
+                            </IconWrap>
+                            <IconWrap>
+                                <FaBootstrap size={iconSize} />
+                            </IconWrap>
                         </TechUsed>
                     </Footer>
                 </ProjectEntry>
                 <ProjectEntry maxW="sm" borderWidth="1px" borderRadius="lg" overflow="hidden">
-                    <ProjectPic src={mangadex} alt={'Mangadex'} />
+                    <ProjectPic src={zappay} alt={'Zap Pay'} />
                     <Content>
                         <Title fontSize='2xl'>
-                            Mangadexterity
+                            Zap Pay
                         </Title>
                         <Description fontSize='lg'>
-                            A web-app that allows the users to read any manga (Japanese comics) series from an online manga reader called Mangadex. All the information and the pages of the manga series are retrieved from the Mangadex API.
+                        For HackGT 2019, my team created a mobile application and an iMessage extension that provides a secure peer-to-peer banking experience. Inspired from platforms like Venmo and Apple Pay, we explored new ways to make easy, quick, and secure transfers.
                         </Description>
                     </Content>
                     <Footer>
-                        <ActionButton colorScheme='blue'>
-                            Github Repo
+                        <ActionButton
+                            rightIcon={<FaCode size={20} />}
+                            colorScheme='blackAlpha'
+                            onClick={() => {
+                                const win = window.open('https://devpost.com/software/zap-pay', '_blank')!;
+                                win.focus();
+                            }}
+                        >
+                            Devpost
                         </ActionButton>
                         <TechUsed fontSize='lg'>
-                            React, Node, Express.js, Bootstrap
+                            <IconWrap>
+                                <FaPython size={iconSize} />
+                            </IconWrap>
+                            <IconWrap>
+                                <FaSwift size={iconSize} />
+                            </IconWrap>
                         </TechUsed>
                     </Footer>
                 </ProjectEntry>
                 <ProjectEntry maxW="sm" borderWidth="1px" borderRadius="lg" overflow="hidden">
-                    <ProjectPic src={mangadex} alt={'Mangadex'} />
+                    <ProjectPic src={rgb} alt={'RGB Color Guess'} />
                     <Content>
                         <Title fontSize='2xl'>
-                            Mangadexterity
+                            RGB Color Guessing Game
                         </Title>
                         <Description fontSize='lg'>
-                            A web-app that allows the users to read any manga (Japanese comics) series from an online manga reader called Mangadex. All the information and the pages of the manga series are retrieved from the Mangadex API.
+                            A simple web-based game where the user guesses the color based on the given RGB. With two difficulties to play with, this game will test your perception of color through the RGB system!
                         </Description>
                     </Content>
                     <Footer>
-                        <ActionButton colorScheme='blue'>
+                        <ActionButton
+                            rightIcon={<FaGithub size={20} />}
+                            colorScheme='blackAlpha'
+                            onClick={() => {
+                                const win = window.open('https://github.com/Masasasaki/RGB-Game', '_blank')!;
+                                win.focus();
+                            }}
+                        >
                             Github Repo
                         </ActionButton>
                         <TechUsed fontSize='lg'>
-                            React, Node, Express.js, Bootstrap
+                            <IconWrap>
+                                <SiJavascript size={iconSize} />
+                            </IconWrap>
+                            <IconWrap>
+                                <FaHtml5 size={iconSize} />
+                            </IconWrap>
+                            <IconWrap>
+                                <FaCss3Alt size={iconSize} />
+                            </IconWrap>
                         </TechUsed>
                     </Footer>
                 </ProjectEntry>
@@ -150,8 +245,14 @@ const Description = styled(Text)`
 `;
 
 const TechUsed = styled(Text)`
+    display: flex;
+    flex-direction: row;
     background: #F5F5F5;
     padding: 10px;
+`;
+
+const IconWrap = styled.div`
+    padding: 0px 5px 0px 5px;
 `;
 
 const Footer = styled(Box)`
@@ -159,7 +260,9 @@ const Footer = styled(Box)`
 `;
 
 const ActionButton = styled(Button)`
-    margin: 10px;
+    margin: 10px 10px 20px 10px;
+    background: black;
+    color: white;
 `;
 
 export { Projects };
